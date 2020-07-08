645c9d3d0d0ef9f93aa2d45c78dcfe70e67b98f4
Heather Peterson <hpeterson462@gmail.com>
2020-07-07 18:04:30 -0700

    tests working

--- test/example.test.js
+++ test/example.test.js
@@ -1,24 +1,30 @@
 // IMPORT MODULES under test here:
 // import example from '../example.js';
-
-const { compareNumbers } = require("../utilis");
+import { compareNumbers } from '../utilis.js';
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const guess1 = 1;
-    const guess2 = 2;
-    const guess3 = 3;
-    const expected = true;
+    const answer1 = 5;
+    const answer2 = 2;
+    const answer3 = 8;
+
+    const expected1 = 3;
+    const expected2 = 4;
+    const expected3 = 8;
 
     //Act 
     // Call the function you're testing and set the result to a const
-    const acutal1 = compareNumbers(guess1);
-    const actual = false;
+    const actual1 = compareNumbers(answer1, expected1);
+    const actual2 = compareNumbers(answer2, expected2);
+    const actual3 = compareNumbers(answer3, expected3);
+
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual1, 1);
+    expect.equal(actual2, -1);
+    expect.equal(actual3, 0);
 });

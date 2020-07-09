02d1525ff5cc95e5703d41f0eee4e722744e313c
Heather Peterson <hpeterson462@gmail.com>
2020-07-09 16:55:00 -0700

    refactoring

--- app.js
+++ app.js
@@ -5,43 +5,35 @@ const quizButton = document.getElementById('quiz-button');
 
 const userGuessSpan = document.getElementById('user-guess');
 
-const userResult = document.getElementById('user-result');
+const userResultSpan = document.getElementById('user-result');
 
 // initialize state
 let randomNumber = Math.ceil(Math.random() * 20);
-console.log(randomNumber);
 
 let userCount = 4;
 
 // set event listeners to update state and DOM
 quizButton.addEventListener('click', () => {
 
-    if (userCount === -1) {
-        return document.getElementById("user-result").textContent = 'Good Game.';
+    if (userCount > 0) {
+        const userInput = Number(prompt('Guess a number between 1 & 20.'));
+
+        if (userInput === 0) {
+            return;
+        }
+
+        const userResults = compareNumbers(userInput, randomNumber);
+        if (userResults === 'Please guess again.') {
+            alert('This isn\'t a number. Try again.');
+        } else {
+            userResultSpan.textContent = userResults;
+        }
+        userCount--;
+        userGuessSpan.textContent = userCount;
+
+        if (userCount === 0) {
+            return document.getElementById('user-result').textContent = 'Good Game.';
+        }
     }
-
-    const userInput = Number(prompt('Guess a number between 1 & 20.'));
-
-    if (userInput === false) {
-        return;
-    }
-
-    if (compareNumbers(userInput, randomNumber) === 0) {
-        userResult.textContent = 'You win!';
-    } else if (compareNumbers(userInput, randomNumber) === -1) {
-        userResult.textContent = 'Too low';
-    } else if (compareNumbers(userInput, randomNumber) === 1) {
-        userResult.textContent = 'Too high';
-    } else {
-        alert('This isn\'t a number. Try again.');
-    }
-
-    userGuessSpan.textContent = userCount--;
-
-    if (userCount === -1) {
-        return document.getElementById('user-result').textContent = 'Sorry, you lost.';
-    }
-
-
 });
 
--- guess.js
+++ guess.js
@@ -1,11 +1,11 @@
 export function compareNumbers(guess, correctNumber) {
 
     if (correctNumber === guess) {
-        return 0;
+        return 'You win!';
     } else if (correctNumber < guess) {
-        return 1;
+        return 'Too high';
     } else if (correctNumber > guess) {
-        return -1;
+        return 'Too low';
     } else {
         return 'Please guess again.';
     }
